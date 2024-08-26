915be1d9f591b2fbf2b63554c5ad863b9258715a
JordonPhillips <JordonPhillips@users.noreply.github.com>
2024-08-26 16:22:08 +0200

    Create a gradle-based dependant package

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,25 @@
 plugins {
     `java-library`
     id("software.amazon.smithy.gradle.smithy-jar")
-    `maven-publish`
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/JordonPhillips/smithy-test-package")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            groupId = "me.jkphillips"
-            artifactId = "smithy-test-packge"
-            version = "1.0"
-            from(components["java"])
-        }
-    }
 }
 
 repositories {
     mavenLocal()
     mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/JordonPhillips/smithy-test-package")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 dependencies {
     val smithyVersion: String by project
 
     smithyCli("software.amazon.smithy:smithy-cli:$smithyVersion")
+    implementation("me.jkphillips:smithy-test-packge:1.0")
 
     // Uncomment below to add various smithy dependencies (see full list of smithy dependencies in https://github.com/awslabs/smithy)
     // implementation("software.amazon.smithy:smithy-model:$smithyVersion")
