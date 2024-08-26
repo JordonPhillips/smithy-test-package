b9401cc594a14a80032edff91a6d099a68836cea
JordonPhillips <JordonPhillips@users.noreply.github.com>
2024-08-26 15:48:06 +0200

    Add GH publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,25 @@
 plugins {
     `java-library`
     id("software.amazon.smithy.gradle.smithy-jar")
+    `maven-publish`
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/JordonPhillips/smithy-test-package")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
 
 repositories {
